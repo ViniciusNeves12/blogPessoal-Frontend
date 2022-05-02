581682b7b246b3bcde1c20e6a9d0dafecdb5cb47
ViniciusNeves12 <viniciusneves1312@hotmail.com>
2022-05-02 16:45:41 -0300

    Arrumando Navbar E Footer

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,22 @@ import React from 'react';
 import Home from './paginas/home/Home';
 import './App.css';
 import { Grid } from '@material-ui/core';
+import Navbar from './components/static/navbar/Navbar';
+import Footer from './components/static/footer/Footer';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <Home></Home>
+    <Router>
+      <Navbar />
+      <div style={{ minHeight: '100vh' }}>
+        <Routes> // Antigo Switch
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </div>
+      <Footer />
+    </Router>
+    
   );
 }
 
---
+++ src/components/static/navbar/Navbar.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { AppBar, Box, Toolbar, Typography } from "@material-ui/core";
+import { Link } from "react-router-dom";
+import './Navbar.css';
+
+function Navbar() {
+    return (
+        <>
+            <AppBar position="static">
+                <Toolbar variant="dense">
+                    <Box className="cursor" >
+                        <Typography variant="h5" color="inherit">
+                            Blog Pessoal
+                        </Typography>
+                    </Box>
+                    <Box display="flex" justifyContent="start">
+                        <Link to='/home' className='text-decoration-none'>
+                            <Box mx={1} className="cursor">
+                                <Typography variant="h6" color="inherit">
+                                    home
+                                </Typography>
+                            </Box>
+                        </Link>
+                        <Box mx={1} className="cursor">
+                            <Typography variant="h6" color="inherit">
+                                postagens
+                            </Typography>
+                        </Box>
+                        <Box mx={1} className="cursor">
+                            <Typography variant="h6" color="inherit">
+                                temas
+                            </Typography>
+                        </Box>
+                        <Box mx={1} className="cursor">
+                            <Typography variant="h6" color="inherit">
+                                cadastrar tema
+                            </Typography>
+                        </Box>
+                        <Link to='/login' className='text-decoration-none'>
+                            <Box mx={1} className="cursor">
+                                <Typography variant="h6" color="inherit">
+                                    logout
+                                </Typography>
+                            </Box>
+                        </Link>
+                    </Box>
+                </Toolbar>
+            </AppBar>
+        </>
+    )
+}
+
+export default Navbar;
