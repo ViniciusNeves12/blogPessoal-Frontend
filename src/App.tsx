6eb589faaa980d3ea33d33449cc304f67f603e38
ViniciusNeves12 <viniciusneves1312@hotmail.com>
2022-05-02 15:37:20 -0300

    Frontend

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Home from './paginas/home/Home';
+import './App.css';
+import { Grid } from '@material-ui/core';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <Home></Home>
+  );
+}
+
+export default App;
